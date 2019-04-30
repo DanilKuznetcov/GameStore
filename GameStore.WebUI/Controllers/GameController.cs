70da372ea5d55347dd4a7d05cfd6ba08211ac15d
DanilKuznetcov <qspirit99@gmail.com>
2019-04-30 17:58:26 +0300

    feature pagination

--- GameStore.WebUI/Controllers/GameController.cs
+++ GameStore.WebUI/Controllers/GameController.cs
@@ -6,21 +6,24 @@ using System.Web.Mvc;
 using GameStore.Domain.Abstract;
 using GameStore.Domain.Entities;
 
-
 namespace GameStore.WebUI.Controllers
 {
     public class GameController : Controller
     {
         private IGameRepository repository;
+        public int pageSize = 6;
+
         public GameController(IGameRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Games);
+            return View(repository.Games
+                .OrderBy(game => game.GameId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize));
         }
-
     }
 }
