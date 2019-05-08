320b44aca8d82a3024c84e39a7901cb4cff11867
DanilKuznetcov <qspirit99@gmail.com>
2019-05-08 08:41:43 +0300

    add authentication for admin

--- GameStore.WebUI/Controllers/AdminController.cs
+++ GameStore.WebUI/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 
 namespace GameStore.WebUI.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         IGameRepository repository;
@@ -41,6 +42,23 @@ namespace GameStore.WebUI.Controllers
                 // Что-то не так со значениями данных
                 return View(game);
             }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Game());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int gameId)
+        {
+            Game deletedGame = repository.DeleteGame(gameId);
+            if (deletedGame != null)
+            {
+                TempData["message"] = string.Format("Игра \"{0}\" была удалена",
+                    deletedGame.Name);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
